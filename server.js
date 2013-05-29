1eebfc3b19bf3a0b21aae11dbafd6d16f22d98bc
ForbesLindesay <forbes@lindesay.co.uk>
2013-05-29 18:58:37 +0100

    Upgrade version of stop and do versioning

--- server.js
+++ server.js
@@ -1,17 +1,41 @@
-var Stop = require('stop');
-var app = new Stop(process.argv[2] === '--compile' || process.argv[2] === '-c');
+var path = require('path')
+var fs = require('fs')
 
-var browserify = require('browserify-middleware');
+var browserify = require('browserify-middleware')
+var express = require('express')
+var app = express()
 
-browserify.settings('transform', ['rfileify']);
+var lsVersion = require('./package.json').dependencies['lexical-scope']
+var version = require('./package.json').version
 
-app.favicon('./favicon.ico');
-app.file('/', './index.html');
-app.get('/client.js', browserify('./client.js'));
-app.file('/codemirror.css', 'code-mirror/codemirror.css');
-app.file('/theme.css', 'code-mirror/theme/solarized.css');
-app.file('/foundation.css', './foundation.css');
-app.file('/background.png', './background.png');
-app.file('/forkme.png', './forkme.png');
+browserify.settings('transform', ['rfileify'])
 
-app.run('./out', 3000);
+app.use(express.logger('dev'))
+app.use(express.favicon('./favicon.ico'))
+
+app.get('/', function (req, res) {
+  res.type('html')
+  var html = fs.readFileSync(__dirname + '/index.html', 'utf8')
+  html = html.replace(/{{lexicalscopeversion}}/g, lsVersion)
+             .replace(/{{version}}/g, version)
+  res.send(html)
+})
+app.get('/static/' + version + '/foundation.css', function (req, res) {
+  res.type('css')
+  var css = fs.readFileSync(__dirname + '/foundation.css', 'utf8')
+  css = css.replace(/{{version}}/g, version)
+  res.send(css)
+})
+app.get('/static/' + version + '/client.js', browserify('./client.js'))
+app.get('/static/' + version + '/codemirror.css', file('./node_modules/code-mirror/codemirror.css'))
+app.get('/static/' + version + '/theme.css', file('./node_modules/code-mirror/theme/solarized.css'))
+app.get('/static/' + version + '/background.png', file('./background.png'))
+app.get('/static/' + version + '/forkme.png', file('./forkme.png'))
+
+function file(path) {
+  return function (req, res, next) {
+    res.sendfile(path)
+  }
+}
+
+app.listen(3000)
